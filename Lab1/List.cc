baed07ee1a26f16ec59829725b0b07512ca3e91f
melgu374 <melgu374@student.liu.se>
2024-04-04 20:31:03 +0200

    oh shit

--- Lab1/List.cc
+++ Lab1/List.cc
@@ -5,24 +5,12 @@
 #include <memory>
 // blev jobbigt att testa minnesläckor med drmemory, då catch.hpp verisionen som kursen använder (2017) har minnesläckor. hittade nog däremot en läcka som beror på min tail (fungerar kanske ej som en sentinel borde) fråga om detta på labben
 
-// struct List::Node
-// {
-//     Node() = default;
-//     Node(int v, Node *p, std::unique_ptr<Node> n)
-//         : value{v}, prev{p}, next{std::move(n)} {}
-//     int value{};
-
-//     // prev ej nödvändig för unique_ptr
-//     // eftersom prev endast är för att hålla koll på föregående nod
-//     Node *prev{};
-//     std::unique_ptr<Node> next{};
-// };
-
 List::List()
     // head äger en resurs, tail pekar på en resurs, därför bör head vara en unique_ptr
-    : head{std::make_unique<Node>()}, tail{}, sz{}
+    : head{std::make_unique<Node>()}, tail(new Node()), sz{}
 {
     head->next = std::make_unique<Node>(0, head.get(), nullptr);
+
     tail = head->next.get();
 }
 
@@ -53,6 +41,7 @@ void List::push_front(int value)
 {
     std::unique_ptr<Node> old_first{std::move(head->next)};
     head->next = std::make_unique<Node>(value, head.get(), std::move(old_first));
+
     old_first->prev = head->next.get();
     ++sz;
 }
@@ -129,108 +118,108 @@ List &List::operator=(List &&rhs) & noexcept
     return *this;
 }
 
-List::List_iterator List::begin() const
-{
-    List_iterator it{};
-    it.pos = head->next.get();
-    return it;
-}
+// List::List_iterator List::begin() const
+// {
+//     List_iterator it{};
+//     it.pos = head->next.get();
+//     return it;
+// }
 
-// end iterator
-List::List_iterator List::end() const
-{
-    List_iterator it{};
-    it.pos = tail;
-    return it;
-}
-List::List_iterator &List::List_iterator::operator++()
-{
-    if (pos != nullptr)
-        pos = pos->next.get();
-    return *this;
-}
-List::List_iterator List::List_iterator::operator++(int)
-{
-    List_iterator temp = *this;
-    ++(*this); // kallar prefix increment
-    return temp;
-}
+// // end iterator
+// List::List_iterator List::end() const
+// {
+//     List_iterator it{};
+//     it.pos = tail;
+//     return it;
+// }
+// List::List_iterator &List::List_iterator::operator++()
+// {
+//     if (pos != nullptr)
+//         pos = pos->next.get();
+//     return *this;
+// }
+// List::List_iterator List::List_iterator::operator++(int)
+// {
+//     List_iterator temp = *this;
+//     ++(*this); // kallar prefix increment
+//     return temp;
+// }
 
-// överbelastning för prefix --
-List::List_iterator &List::List_iterator::operator--()
-{
-    if (pos != nullptr)
-        pos = pos->prev;
-    return *this;
-}
-List::List_iterator List::List_iterator::operator--(int)
-{
-    List_iterator temp = *this;
-    --(*this); // kallar prefix decrement
-    return temp;
-}
+// // överbelastning för prefix --
+// List::List_iterator &List::List_iterator::operator--()
+// {
+//     if (pos != nullptr)
+//         pos = pos->prev;
+//     return *this;
+// }
+// List::List_iterator List::List_iterator::operator--(int)
+// {
+//     List_iterator temp = *this;
+//     --(*this); // kallar prefix decrement
+//     return temp;
+// }
 
-List::List_iterator &List::List_iterator::operator=(const List::List_iterator &other)
-{
-    if (this != &other)
-    {
-        pos = other.pos;
-    }
-    return *this;
-}
-// jämförelseoperator
-bool List::List_iterator::operator==(List_iterator const &it) const
-{
-    return pos == it.pos;
-}
-bool List::List_iterator::operator!=(List_iterator const &it) const
-{
-    return pos != it.pos;
-}
-// överbelastning för dereferensoperatorn
-int &List::List_iterator::operator*() const
-{
-    // kan lika gärna göra de här checkarna i begin och end
-    //  if (pos == nullptr)
-    //  {
-    //      throw out_of_range{"Unitiated List_iterator"};
-    //  }
-    //  else if (pos->next == nullptr)
-    //  {
-    //      throw out_of_range{"Index out of range"};
-    //  }
-    //  else
-    //  {
-    return pos->value;
-    // }
-}
-List::List_iterator List::insert(List_iterator position, const int &N)
-{
-    // kolla ifall insert är i början
-    if (position.pos == head->next.get())
-    {
-        push_front(N);
-        return List_iterator{head->next.get()};
-    }
+// List::List_iterator &List::List_iterator::operator=(const List::List_iterator &other)
+// {
+//     if (this != &other)
+//     {
+//         pos = other.pos;
+//     }
+//     return *this;
+// }
+// // jämförelseoperator
+// bool List::List_iterator::operator==(List_iterator const &it) const
+// {
+//     return pos == it.pos;
+// }
+// bool List::List_iterator::operator!=(List_iterator const &it) const
+// {
+//     return pos != it.pos;
+// }
+// // överbelastning för dereferensoperatorn
+// int &List::List_iterator::operator*() const
+// {
+//     // kan lika gärna göra de här checkarna i begin och end
+//     //  if (pos == nullptr)
+//     //  {
+//     //      throw out_of_range{"Unitiated List_iterator"};
+//     //  }
+//     //  else if (pos->next == nullptr)
+//     //  {
+//     //      throw out_of_range{"Index out of range"};
+//     //  }
+//     //  else
+//     //  {
+//     return pos->value;
+//     // }
+// }
+// List::List_iterator List::insert(List_iterator position, const int &N)
+// {
+//     // kolla ifall insert är i början
+//     if (position.pos == head->next.get())
+//     {
+//         push_front(N);
+//         return List_iterator{head->next.get()};
+//     }
 
-    // kolla ifall position är i slutt eller oinitierat
-    else if (position.pos == tail || position.pos == nullptr)
-    {
-        push_back(N);
-        // returna det vi nyss inserta
-        return List_iterator{tail->prev};
-    }
-    else
-    {
-        // ny nod
-        auto new_node = std::make_unique<Node>(N);
+//     // kolla ifall position är i slutt eller oinitierat
+//     else if (position.pos == tail || position.pos == nullptr)
+//     {
+//         push_back(N);
+//         // returna det vi nyss inserta
+//         return List_iterator{tail->prev};
+//     }
+//     else
+//     {
+//         // ny nod
+//         auto new_node = std::make_unique<Node>(N);
 
-        Node *prev_node = position.pos->prev; // säkert eftersom position är inte head->next eller  nullptr
-        new_node->prev = prev_node;
-        new_node->next = std::move(prev_node->next);
-        position.pos->prev = new_node.get();
-        new_node->prev->next = std::move(new_node);
+//         Node *prev_node = position.pos->prev; // säkert eftersom position är inte head->next eller  nullptr
+//         new_node->prev = prev_node;
+//         new_node->next = std::move(prev_node->next);
+//         position.pos->prev = new_node.get();
+//         new_node->prev->next = std::move(new_node);
 
-        return List_iterator{position.pos->prev};
-    }
-}
+//         return List_iterator{position.pos->prev};
+//     }
+// }
--- Lab1/list-test.cc
+++ Lab1/list-test.cc
@@ -8,43 +8,89 @@
 TEST_CASE("Create list")
 {
     List lst{1, 4, 2, 6, 8, 9};
-    CHECK(lst.at(2) == 2);
-    CHECK(lst.size() == 6);
-    List l2;
+    // CHECK(lst.at(2) == 2);
+    // CHECK(lst.size() == 6);
+    List l2{};
     l2 = lst;
     CHECK(l2.size() == lst.size());
     CHECK(l2.front() == lst.front());
     CHECK(l2.back() == lst.back());
 }
+// TEST_CASE("List Construction and Basic Access")
+// {
+//     // Test default constructor
+//     List emptyList;
+//     CHECK(emptyList.size() == 0);
+//     CHECK(emptyList.empty() == true);
+
+//     // Test initializer list constructor
+//     List initList{1, 2, 3, 4};
+//     CHECK(initList.size() == 4);
+//     CHECK(initList.empty() == false);
+//     CHECK(initList.front() == 1);
+//     CHECK(initList.back() == 4);
+
+//     // Test copy constructor
+//     List copiedList = initList;
+//     CHECK(copiedList.size() == initList.size());
+//     CHECK(copiedList.front() == initList.front());
+//     CHECK(copiedList.back() == initList.back());
+
+//     // Test move constructor
+//     List movedList = std::move(copiedList);
+//     CHECK(movedList.size() == 4);
+//     CHECK(copiedList.size() == 0); // Assuming it's cleared
+//     CHECK(movedList.front() == 1);
+//     CHECK(movedList.back() == 4);
+// }
+// TEST_CASE("Push Operations")
+// {
+//     List l3{};
+
+//     // Test push_front
+//     l3.push_front(1);
+//     l3.push_front(2);
+//     CHECK(l3.size() == 2);
+//     CHECK(l3.front() == 2);
+//     CHECK(l3.back() == 1);
+
+//     // Test push_back
+//     l3.push_back(3);
+//     l3.push_back(4);
+//     CHECK(l3.size() == 4);
+//     CHECK(l3.front() == 2);
+//     CHECK(l3.back() == 4);
+//     std::cout << "l3: ";
+// }
 
 // TEST_CASE("Iterator Backwards")
 // {
 //     List lst{2, 3, 1, 5};
-//     auto rb{make_reverse_iterator(lst.end())};
-//     auto re{make_reverse_iterator(lst.begin())};
+//     auto rb{std::make_reverse_iterator(lst.end())};
+//     auto re{std::make_reverse_iterator(lst.begin())};
 //     for (auto it = rb; it != re; ++it)
 //     {
-//         cout << *it << ' ';
+//         std::cout << *it << ' ';
 //     }
 // }
 
-TEST_CASE("Iterator Reverse")
-{
-    List lst{2, 3, 1, 5};
-    auto rb = std::make_reverse_iterator(lst.end());
-    auto re = std::make_reverse_iterator(lst.begin());
+// TEST_CASE("Iterator Reverse")
+// {
+//     List lst{2, 3, 1, 5};
+//     auto rb = std::make_reverse_iterator(lst.end());
+//     auto re = std::make_reverse_iterator(lst.begin());
 
-    // Use a vector to collect the elements in reverse order
-    std::vector<int> elements;
-    for (auto it = rb; it != re; ++it)
-    {
-        elements.push_back(*it);
-    }
+//     // Use a vector to collect the elements in reverse order
+//     std::vector<int> elements;
+//     for (auto it = rb; it != re; ++it)
+//     {
+//         elements.push_back(*it);
+//     }
 
-    // Now, verify the order of elements is as expected
-    REQUIRE(elements.size() == 4); // Expect 4 elements
-    REQUIRE(elements[0] == 5);     // The first element in reverse order should be 5
-    REQUIRE(elements[1] == 1);     // And so on...
-    REQUIRE(elements[2] == 3);
-    REQUIRE(elements[3] == 2);
-}
+//     // Now, verify the order of elements is as expected
+//     REQUIRE(elements.size() == 4); // Expect 4 elements
+//     REQUIRE(elements[0] == 5);     // The first element in reverse order should be 5
+//     REQUIRE(elements[1] == 1);     // And so on...
+//     REQUIRE(elements[2] == 3);
+//     REQUIRE(elements[3] == 2);
+// }
